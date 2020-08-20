6ac24e3ef614198c8d3c5041a17628cbc93e2619
junior <13500154+kjuniors2@users.noreply.github.com>
2020-08-20 12:25:39 -0300

    Utilizado atibuto complexo filhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,10 +1,18 @@
 class Pessoa:
+    def __init__(self, *filhos, nome=None, idade=None):
+        self.idade = idade
+        self.nome = nome
+        self.filhos = list(filhos)
+
+
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
 if __name__ == '__main__':
-    p = Pessoa()
-    print(Pessoa.cumprimentar(p))
-    print(id(p))
-    print(p.cumprimentar())
-
+    joao = Pessoa(nome='Joao')
+    julio = Pessoa(joao, nome='Julio')
+    print(julio.nome)
+    print(joao.nome)
+    print(julio.filhos)
+    for filho in julio.filhos:
+        print(filho.nome)
