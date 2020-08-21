8dfb1e3fc0920c0485bf19ee6f4bde98e1ff9243
junior <13500154+kjuniors2@users.noreply.github.com>
2020-08-20 23:52:01 -0300

    Atributos da classe e métodos da classe

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,8 +1,18 @@
 class Pessoa:
+    olhos = 2
+
     def __init__(self, *filhos, nome=None, idade=None):
         self.idade = idade
         self.nome = nome
         self.filhos = list(filhos)
+
+    @staticmethod
+    def metodoestatico():
+        return 42
+
+    @classmethod
+    def classemetodo(cls):
+        return f'{cls} - olhos {cls.olhos}'
 
 
     def cumprimentar(self):
@@ -16,3 +26,14 @@ if __name__ == '__main__':
     print(julio.filhos)
     for filho in julio.filhos:
         print(filho.nome)
+    julio.sobrenome = 'Kimichi'
+    print(julio.sobrenome)
+    del joao.filhos
+    julio.olhos = 3
+    print(joao.__dict__)
+    print(julio.__dict__)
+    print(julio.olhos)
+    print(joao.olhos)
+    print(id(Pessoa.olhos), id(julio.olhos), id(joao.olhos))
+    print(Pessoa.metodoestatico(), julio.metodoestatico())
+    print(Pessoa.classemetodo(), julio.classemetodo())
